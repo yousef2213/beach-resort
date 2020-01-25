b8ae3c6dbc736d7dbbb0c1ed14cf60f0f5bc7ff9
yousef2213 <mandooyousef988@gmail.com>
2020-01-25 11:36:35 +0200

    new

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <div className="footer">
-           &copy; all right reserved | design bt yousef ayman
+            &copy; all right reserved | design by yousef ayman
         </div>
     )
 }
